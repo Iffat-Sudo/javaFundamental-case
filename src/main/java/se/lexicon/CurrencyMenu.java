666b47b2f1b8a3ac77eb8c0987a3633a902f5b0e
Iffat-Sudo <zabin.ifa@gmail.com>
2025-10-16 11:51:46 +0200

    Added class CurrencyMenu

---
+++ src/main/java/se/lexicon/CurrencyMenu.java
@@ -0,0 +1,13 @@
+package se.lexicon;
+
+public class CurrencyMenu {
+    public static void displayCurrencyMenu() {
+        System.out.println("== Currency Converter App ==");
+        System.out.println("1. Convert SEK to USD");
+        System.out.println("2. Convert USD to SEK");
+        System.out.println("3. Convert SEK to EUR");
+        System.out.println("4. Convert EUR to SEK");
+        System.out.println("0. Exit");
+        System.out.println("Enter your choice:");
+    }
+}
