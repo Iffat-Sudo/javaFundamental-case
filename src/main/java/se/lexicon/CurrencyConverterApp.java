c2f288677dc7e987ed510a4071093f92283aa612
Iffat-Sudo <zabin.ifa@gmail.com>
2025-10-17 11:05:15 +0200

    Added CurrencyConverterApp.java

--- src/main/java/se/lexicon/CurrencyConverterApp.java
+++ src/main/java/se/lexicon/CurrencyConverterApp.java
@@ -1,4 +1,58 @@
 package se.lexicon;
 
 public class CurrencyConverterApp {
+    public static void main(String[] args) {
+        CurrencyMenu CurrencyMenu = new CurrencyMenu();
+        InputHandler input = new InputHandler();
+        Formatter formatter = new Formatter();
+        ExchangeRates rates = new ExchangeRates();
+
+        boolean running = true;
+        while (running) {
+            CurrencyMenu.displayCurrencyMenu();
+            String choice = input.getMenuChoice();
+
+            switch (choice) {
+                case "1": {
+                    double amount = input.getAmount();
+                    double result = ExchangeRates.convert("SEK", "USD", amount);
+                    formatter.printResult(amount, result, "SEK", "USD");
+                    break;
+                }
+                case "2": {
+                    double amount = input.getAmount();
+                    double result = ExchangeRates.convert("USD", "SEK", amount);
+                    formatter.printResult(amount, result, "USD", "SEK");
+                    break;
+                }
+                case "3": {
+                    double amount = input.getAmount();
+                    double result = ExchangeRates.convert("SEK", "EUR", amount);
+                    formatter.printResult(amount, result, "SEK", "EUR");
+                    break;
+                }
+                case "4": {
+                    double amount = input.getAmount();
+                    double result = ExchangeRates.convert("EUR", "SEK", amount);
+                    formatter.printResult(amount, result, "EUR", "SEK");
+                    break;
+                }
+                case "5":
+                    rates.displayRates();
+                    break;
+
+                case "6":
+                    System.out.println("Goodbye!");
+                    running = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid choice");
+            }
+
+
+
+        }
+
+    }
 }
